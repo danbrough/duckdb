b35722ad7a1c68bea0dc0377a6b584c5faa9b289
Dan Brough <dan@danbrough.org>
2024-06-24 13:20:11 +1200

    android34_x86_64 compiling but with no extensions

---
+++ android_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+
+
+export ANDROID_NDK=/mnt/files/sdk/android/ndk/26.3.11579264
+export ANDROID_API=34
+#ANDROID_ABI=arm64-v8a
+export ANDROID_ABI=x86_64
+
+
--- build.sh
+++ build.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
 
+source android_env.sh
 
-
-#export NDKDIR=/mnt/files/sdk/android/ndk/25.2.9519653
-#export NDKDIR=/mnt/files/sdk/android/ndk/24.0.8215888
-
-export ANDROID_NDK=/mnt/files/sdk/android/ndk/26.3.11579264
-export ANDROID_API=34
-#ANDROID_ABI=arm64-v8a
-export ANDROID_ABI=x86_64
 export DUCKDB_PLATFORM=android${ANDROID_API}_${ANDROID_ABI}
-
-
 export BUILD_ICU=0
 export BUILD_PARQUET=0
+export BUILD_UNITTESTS=false
 
-GEN=ninja make | tee build_${DUCKDB_PLATFORM}.log 2>&1
+GEN=ninja make -d | tee build_${DUCKDB_PLATFORM}.log 2>&1
 
 
 
