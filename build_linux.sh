0c75799a102275c19a86e60d5857bed7187e5861
Dan Brough <dan@danbrough.org>
2024-06-25 07:50:21 +1200

    update

--- build_linux.sh
+++ build_linux.sh
@@ -2,11 +2,12 @@
 
 
 
+export BUILD_JEMALLOC=1
+export BUILD_HTTPFS=1
+export BUILD_JSON=1
 export BUILD_ICU=1
-export BUILD_PARQUET=0
-export DISABLE_PARQUET=1
+export BUILD_PARQUET=1
 export BUILD_UNITTESTS=false
-export DISABLE_BUILTIN_EXTENSIONS=TRUE
 
 GEN=ninja make release_original  | tee build_linux.log 2>&1
 
