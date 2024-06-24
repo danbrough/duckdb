59ef0316926355f9d937e7fb1a8f2da1cccfd165
Dan Brough <dan@mac.local>
2024-06-24 16:46:08 +1200

    update

--- android_env.sh
+++ android_env.sh
@@ -2,8 +2,12 @@
 
 
 
+if [ -z "$ANDROID_NDK" ]; then 
+	export ANDROID_NDK=/mnt/files/sdk/android/ndk/26.3.11579264
+	echo ANDROID_NDK not set using default $ANDROID_NDK
+fi
 
-export ANDROID_NDK=/mnt/files/sdk/android/ndk/26.3.11579264
+	
 export ANDROID_API=34
 #ANDROID_ABI=arm64-v8a
 export ANDROID_ABI=x86_64
